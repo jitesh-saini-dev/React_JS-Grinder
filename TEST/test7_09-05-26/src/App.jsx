d455a854d4d690c0facb25994d9a18569d5af1b4
jitesh <iamjiteshsaini2004@gmail.com>
2026-08-29 23:22:20 +0530

    Remove commented imports and clean up code in App and main components

--- TEST/test7_09-05-26/src/App.jsx
+++ TEST/test7_09-05-26/src/App.jsx
@@ -1,4 +1,3 @@
-// import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Component/Header";
 import Home from "./component/Home";
--- TEST/test7_09-05-26/src/main.jsx
+++ TEST/test7_09-05-26/src/main.jsx
@@ -1,4 +1,3 @@
-// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -6,10 +5,6 @@ import { Provider } from "react-redux";
 import store from "./Store/store.js";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
-
   <Provider store={store}>
     <App />
   </Provider>,
