ade72ea4d23e760f70474015b3b88cfbab3c28ee
jitesh <iamjiteshsaini2004@gmail.com>
2026-09-06 00:09:38 +0530

    Remove unused React imports from App and Header components

--- TEST/test1/src/App.jsx
+++ TEST/test1/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
--- TEST/test1/src/Home.jsx
+++ TEST/test1/src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 
 const Home = () => {
   const data = localStorage.getItem("userdata");
